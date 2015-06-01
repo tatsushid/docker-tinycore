73d8b3c4afc6bae1afa8f4023c60a76b49a8b649
Tatsushi Demachi <tdemachi@gmail.com>
2015-06-01 21:34:23 +0900

    Add 6.3 build scripts

---
+++ 6.3/x86/src/build_squashfs_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ ! -f squashfs-tools.tcz ]; then
+    wget http://tinycorelinux.net/6.x/x86/tcz/squashfs-tools.tcz
+fi
+
+mount -t squashfs -o loop,ro,bs=4096 squashfs-tools.tcz /mnt
+tar cf - -C /mnt . | gzip -c

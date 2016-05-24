0ee1b4e2c375b4f4fc8ea117a100ece36615b3a3
Tatsushi Demachi <tdemachi@gmail.com>
2016-05-24 20:25:57 +0900

    Fix /mnt not found error on latest busybox image

--- 6.1/x86/src/build_squashfs_tools.sh
+++ 6.1/x86/src/build_squashfs_tools.sh
@@ -4,5 +4,6 @@ if [ ! -f squashfs-tools.tcz ]; then
     wget http://tinycorelinux.net/6.x/x86/tcz/squashfs-tools.tcz
 fi
 
+[ -d /mnt ] || mkdir /mnt
 mount -t squashfs -o loop,ro,bs=4096 squashfs-tools.tcz /mnt
 tar cf - -C /mnt . | gzip -c

d1f6f90f363906356042a298464c1f2cb3dd5c6f
Tatsushi Demachi <tdemachi@gmail.com>
2016-07-08 20:18:07 +0900

    Add 7.2 build scripts

---
+++ 7.2/x86_64/src/build_squashfs_tools.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -f squashfs-tools.tcz ]; then
+    wget http://tinycorelinux.net/7.x/x86_64/tcz/squashfs-tools.tcz
+fi
+
+[ -d /mnt ] || mkdir /mnt
+mount -t squashfs -o loop,ro,bs=4096 squashfs-tools.tcz /mnt
+tar cf - -C /mnt . | gzip -c
